5729eefd664aafc62510ae2adc8650f49048deca
ripper <617498836@qq.com>
2025-10-17 14:21:01 +0800

    feat: update API version to 1.1 in Swagger configuration

--- src/main.ts
+++ src/main.ts
@@ -21,7 +21,7 @@ async function bootstrap(port: number) {
   const config = new DocumentBuilder()
     .setTitle('Cats example')
     .setDescription('The cats API description')
-    .setVersion('1.0')
+    .setVersion('1.1')
     .addTag('cats')
     .build();
   const document = SwaggerModule.createDocument(app, config);
